b8422d10ddd2f5ea3a3ca08bc1998197cee2dc2f
SadovskiiAleks <alekssadovskij@yandex.ru>
2022-12-21 21:34:51 +0300

    Add FunctionalInterface OnTaskErrorListener and OnTaskDoneListener

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        Worker.OnTaskDoneListener listener = System.out::println;
-        Worker.OnTaskErrorListener error = System.out::println;
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener error = System.out::println;
 
         Worker worker2 = new Worker(listener,error);
         worker2.start();
